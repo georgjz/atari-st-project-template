ca45bbc7c9c87dfb69551a94fa5d9c8154253939
Georg Ziegler <georgjziegler@gmail.com>
2019-12-21 15:26:49 +0100

    Remove surplus code

--- src/init.s
+++ src/init.s
@@ -25,21 +25,3 @@ InitBasepage::
 ;----- end of InitBasepage subroutine ------------------------------------------
 
 ;----- end of init section -----------------------------------------------------
-
-
-            ; move.l    4(sp),a0      ; Pointer to BASEPAGE
-            ; lea       mystack,sp    ; Set stack pointer
-            ; move.l    #$100,d0      ; Length of basepage
-            ; add.l     $c(a0),d0     ; Length of the TEXT segment
-            ; add.l     $14(a0),d0    ; Length of the DATA segment
-            ; add.l     $1c(a0),d0    ; Length of the BSS segment
-            ; move.l    d0,-(sp)      ; Return to the stack
-            ; move.l    a0,-(sp)      ; Basepage address to stack
-            ; clr.w     -(sp)         ; Fill parameter
-            ; move.w    #$4a,-(sp)    ; Mshrink
-            ; trap      #1            ; Call GEMDOS
-            ; lea       $c(sp),sp     ; Correct stack
-            ; jsr       main          ; Call main program
-            ; move.w    d0,-(sp)      ; Return value of the program
-            ; move.w    #$4c,-(sp)    ; Pterm
-            ; trap      #1            ; Call GEMDOS
